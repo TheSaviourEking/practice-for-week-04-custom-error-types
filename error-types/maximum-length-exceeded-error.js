b16202fb6b43935851a9298dcb23fa1c82bcd680
TheSaviourEking <saviour.eking@outlook.com>
2023-09-24 01:33:34 +0100

    SOLVED: fix maximum-length-exceeded-error to pass test

--- error-types/maximum-length-exceeded-error.js
+++ error-types/maximum-length-exceeded-error.js
@@ -1,6 +1,18 @@
 const ValidationError = require('./validation-error');
 
 // Your code here
+class MaximumLengthExceededError extends ValidationError {
+  constructor(maxLensubactualLen, ...params) {
+    super(...params);
+
+    // if(Error.captureStackTrace) {
+    //   Error.captureStackTrace(this, MaximumLengthExceededError);
+    // }
+
+    this.name = 'MaximumLengthExceededError';
+    maxLensubactualLen? this.message = `Maximum length exceeded by ${maxLensubactualLen}` : this.message = `Maximum length exceeded`;
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
